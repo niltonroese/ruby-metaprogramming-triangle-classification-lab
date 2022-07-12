8ab233a0a82788e8b036fb00d87381cb1455503c
Nilton <niltonroese@gmail.com>
2022-07-12 20:14:34 +1000

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
   # write code here
+    attr_reader :a, :b, :c
+
+    def initialize(a, b, c)
+      @a = a
+      @b = b
+      @c = c
+    end
+
+    def kind
+      validate_triangle
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+    def validate_triangle
+      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each do |side|
+      #   real_triangle << false if side <= 0 
+      # raise TriangleError if real_triangle.include?(false)
+      if side <= 0
+        real_triangle << false
+      end
+  
+      if real_triangle.include? (false)
+        #begin
+        raise TriangleError
+        #rescue TriangleError => error
+        #puts error.message
+        #end
+      end
+    end
+  end
+  
+    class TriangleError < StandardError
+      def message
+        "Its not a valid triangle!"
+      end
+    end
 end
